e1e61faf3f6e2ec315aa43a1a75feae1b3e9485d
lepakshi <lchundur@extremenetworks.com>
2022-09-05 23:44:40 +0530

    updated

--- docker.sh
+++ docker.sh
@@ -1,5 +1,16 @@
 #!/bin/sh
-
-set -e
+  
 docker build -t lepakshich/test:latest .
+
+docker push lepakshich/test:latest
+
+docker service ps web2
+
+if [ $? -eq 0 ]
+then
+        docker service update --with-registry-auth --force --update-parallelism 1 --update-delay 5s --image lepakshich/test:latest web2
+else
+        docker service create --with-registry-auth --replicas 2 -p 8101:80 --name web2 lepakshich/test:latest
+fi
+
 exit 0
