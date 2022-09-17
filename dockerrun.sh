ad2754f3712170f9f89f4214bea22296bfc4b3d2
lepakshi <lepakshi.chunduri@gmail.com>
2022-09-17 08:24:28 +0000

    updated

---
+++ dockerrun.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+  
+cat dockerpassword | docker login --username lepakshich --password-stdin
+rv=$?
+echo "login rv, $rv"
+
+set +e
+docker rm -f edureka_cert1
+rv=$?
+echo "rm rv, $rv"
+set -e
+
+docker run -d -p 80:80 --name edureka_cert1 lepakshich/test
+echo "run rv, $rv"
+rv=$?
+
+exit $rv
+
+docker push lepakshich/test:latest
+
+docker service ps web2
+
+if [ $? -eq 0 ]
+then
+        docker service update --with-registry-auth --force --update-parallelism 1 --update-delay 5s --image lepakshich/test:latest web2
+else
+        docker service create --with-registry-auth --replicas 2 -p 8101:80 --name web2 lepakshich/test:latest
+fi
+
+exit 0
