f036f989bf98a286b4350f640a221f415bad660f
lepakshi <lepakshi.chunduri@gmail.com>
2022-09-17 08:59:40 +0000

    updated

--- dockerrun.sh
+++ dockerrun.sh
@@ -19,16 +19,3 @@ echo "run rv, $rv"
 rv=$?
 
 exit $rv
-
-docker push lepakshich/test:latest
-
-docker service ps web2
-
-if [ $? -eq 0 ]
-then
-        docker service update --with-registry-auth --force --update-parallelism 1 --update-delay 5s --image lepakshich/test:latest web2
-else
-        docker service create --with-registry-auth --replicas 2 -p 8101:80 --name web2 lepakshich/test:latest
-fi
-
-exit 0
